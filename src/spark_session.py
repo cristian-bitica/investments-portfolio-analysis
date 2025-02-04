7bd26300551bdec68ea041004c17360404797b50
Cristian Bitica <cristian.bitica7@gmail.com>
2025-02-04 06:02:58 +0000

    fix missing packages from session

--- src/spark_session.py
+++ src/spark_session.py
@@ -1,15 +1,18 @@
 from pyspark.sql import SparkSession
 
-def get_spark_session():
 
+def get_spark_session():
     # token is not required for local testing
-    return SparkSession.builder \
-        .appName("local-uc-test") \
-        .master("local[*]") \
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "io.unitycatalog.spark.UCSingleCatalog") \
-        .config("spark.sql.catalog.unity", "io.unitycatalog.spark.UCSingleCatalog") \
-        .config("spark.sql.catalog.unity.uri", "http://localhost:8080") \
-        .config("spark.sql.catalog.unity.token", "") \
-        .config("spark.sql.defaultCatalog", "unity") \
+    return (
+        SparkSession.builder.appName("local-uc-test")
+        .master("local[*]")
+        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.2.1")
+        .config("spark.jars.packages", "io.unitycatalog:unitycatalog-spark_2.12:0.2.0")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config("spark.sql.catalog.spark_catalog", "io.unitycatalog.spark.UCSingleCatalog")
+        .config("spark.sql.catalog.unity", "io.unitycatalog.spark.UCSingleCatalog")
+        .config("spark.sql.catalog.unity.uri", "http://localhost:8080")
+        .config("spark.sql.catalog.unity.token", "")
+        .config("spark.sql.defaultCatalog", "unity")
         .getOrCreate()
+    )
--- tests/test_spark_session.py
+++ tests/test_spark_session.py
@@ -1,3 +1,5 @@
+from pyspark.sql.catalog import Catalog
+
 from src.spark_session import get_spark_session
 
 
@@ -8,4 +10,5 @@ def test_get_spark_session():
     assert spark.conf.get("spark.sql.catalog.unity.token") == ""
     assert spark.conf.get("spark.sql.defaultCatalog") == "unity"
     assert spark.conf.get("spark.sql.extensions") == "io.delta.sql.DeltaSparkSessionExtension"
+    assert Catalog(spark).currentCatalog() == "unity"
     spark.stop()
